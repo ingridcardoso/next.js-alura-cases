dc180de5b483a95a551712a8964bb3efdf494136
ingridcardoso <ingridmarques.cardoso@gmail.com>
2022-12-22 11:44:36 -0300

    initial commit

---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link"
+
+export default function FAQPage() {
+  return (
+    <div>
+      <h2>Alura Cases - Faça a sua pergunta</h2>
+      <Link href="/">Voltar para página Home</Link>
+    </div>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import HomeScreen from '../src/screens/HomeScreen';
+
+export default HomeScreen;
+
+// function Title({ children, as }) {
+//   const Tag = as
+//   return (
+//     <React.Fragment>
+//       <Tag>
+//         {children}
+//       </Tag>
+//       <style jsx>{`
+//         ${Tag} {
+//           color: blue;
+//         }
+//       `}</style>
+//     </React.Fragment>
+//   )
+// }
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as="h1">Alura cases - Welcome to Next.js!</Title>
+//       <Link href="/faq">
+//         Ir para a Página do FAQ
+//       </Link>
+//     </div>
+//   )
+// }
